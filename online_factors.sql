7e651b31bfedcc2c189e1b30894b20dd05ee2f79
okaeiz <100111649+okaeiz@users.noreply.github.com>
2023-09-21 13:49:28 +0330

    unused factors removed
    - Unused factors are removed in this commit.
    - Method D1 & D2 and the reduced versions are included.
    - Data from milestones_ceremonies is now included in online_scores.

--- online_factors.sql
+++ online_factors.sql
@@ -1,10 +1,46 @@
-CREATE OR REPLACE VIEW online_factors AS
-SELECT 
-    os.*, -- This will select all fields from online_scores
-    fetch_e2_value(os.project_id) AS e2_value,
-    os.a_value * ((os.b_value * 0.2) + (os.c_value * 0.2) + (os.d_value * 0.6)) * EXP(fetch_e2_value(os.project_id)) AS methode_b2,
-    fetch_r22_value(os.project_id) as r22_value,
-    os.a_value * ((os.b_value * 0.2) + (os.c_value * 0.2) + (os.d_value * 0.6)) * EXP(fetch_r22_value(os.project_id)) AS methode_a2
+create view public.online_factors
+            (project_id, milestone_id, a_value, b_value, c_value, d_value, e_value, f_value, plan_value, review_value,
+             retro_value, ccs1_value, ccs2_value, methode_c1, methode_d1, methode_d1_red, methode_d2, methode_d2_red)
+as
+SELECT os.project_id,
+       os.milestone_id,
+       os.a_value,
+       os.b_value,
+       os.c_value,
+       os.d_value,
+       os.e_value,
+       os.f_value,
+       os.plan_value,
+       os.review_value,
+       os.retro_value,
+       os.ccs1_value,
+       os.ccs2_value,
+       os.a_value *
+       (os.b_value * 0.2::double precision + os.c_value * 0.2::double precision + os.d_value * 0.5::double precision +
+        os.f_value * 0.1::double precision) * (0.6::double precision + os.e_value) AS methode_c1,
+       os.a_value * COALESCE((os.b_value * 0.2::double precision + os.c_value * 0.2::double precision +
+                              os.d_value * 0.5::double precision + os.f_value * 0.1::double precision) *
+                             (os.e_value / ((SELECT get_project_commitment_ratio.ratio
+                                             FROM get_project_commitment_ratio(os.project_id) get_project_commitment_ratio(id, ratio)))),
+                             0::double precision)                                  AS methode_d1,
+       os.a_value *
+       (os.b_value * 0.2::double precision + os.c_value * 0.2::double precision + os.d_value * 0.5::double precision +
+        os.f_value * 0.1::double precision)                                        AS methode_d1_red,
+       os.plan_value * os.a_value * COALESCE(
+                   (os.b_value * 0.15::double precision + os.c_value * 0.15::double precision +
+                    os.d_value * 0.40::double precision + os.f_value * 0.15::double precision +
+                    os.review_value * 0.06::double precision + os.retro_value * 0.03::double precision +
+                    os.ccs1_value * 0.03::double precision + os.ccs2_value * 0.03::double precision) *
+                   (os.e_value / ((SELECT get_project_commitment_ratio.ratio
+                                   FROM get_project_commitment_ratio(os.project_id) get_project_commitment_ratio(id, ratio)))),
+                   0::double precision)                                            AS methode_d2,
+       os.plan_value * os.a_value * (os.b_value * 0.15::double precision + os.c_value * 0.15::double precision +
+                                     os.d_value * 0.40::double precision + os.f_value * 0.15::double precision +
+                                     os.review_value * 0.06::double precision +
+                                     os.retro_value * 0.03::double precision + os.ccs1_value * 0.03::double precision +
+                                     os.ccs2_value * 0.03::double precision)       AS methode_d2_red
+FROM online_scores os;
 
-FROM 
-    online_scores os;
+alter table public.online_factors
+    owner to taiga_u;
+
--- online_scores.sql
+++ online_scores.sql
@@ -1,32 +1,52 @@
-CREATE OR REPLACE VIEW online_scores AS
-WITH CTE AS (SELECT
-    sp.ref AS project_id,
-    (SELECT "id"
-        FROM "public"."milestones_milestone"
-        WHERE "public"."milestones_milestone"."project_id" = sp.ref
-        ORDER BY "estimated_finish" DESC
-        LIMIT 1
-    ) AS milestone_id,
-    COALESCE(fetch_a_value(sp.ref), 0) AS a_value, -- Assuming you're getting a_value from a function
-    COALESCE(fetch_b_value(sp.ref, sp.task_done_id), 0) AS b_value, -- Assuming you're getting a_value from a function
-    COALESCE(fetch_c_value(sp.ref), 0) AS c_value, -- Assuming c_value is a column in scrum_projects
-    COALESCE(fetch_d_value(sp.ref), 0) AS d_value, -- Assuming d_value is a column in scrum_projects
-    COALESCE(fetch_e_value(sp.ref), 0) AS e_value, -- Assuming e_value is a column in scrum_projects
-    COALESCE( fetch_r2_value(sp.ref), 0) AS r2_value -- Assuming r2_value is a column in scrum_projects
+create view public.online_scores
+            (project_id, milestone_id, a_value, b_value, c_value, d_value, e_value, f_value, plan_value,
+             review_value, retro_value, ccs1_value, ccs2_value)
+as
+WITH cte AS (SELECT sp.ref                                                                AS project_id,
+                    (SELECT milestones_milestone.id
+                     FROM milestones_milestone
+                     WHERE milestones_milestone.project_id = sp.ref
+                       AND now() >= milestones_milestone.estimated_start
+                       AND now() <= milestones_milestone.estimated_finish
+                     ORDER BY milestones_milestone.estimated_finish DESC
+                     LIMIT 1)                                                             AS milestone_id,
+                    COALESCE(fetch_a_value(sp.ref), 0::double precision)                  AS a_value,
+                    COALESCE(fetch_b_value(sp.ref, sp.task_done_id), 0::double precision) AS b_value,
+                    COALESCE(fetch_c_value(sp.ref), 0::double precision)                  AS c_value,
+                    COALESCE(fetch_d_value(sp.ref), 0::double precision)                  AS d_value,
+                    COALESCE(fetch_e_value(sp.ref), 0::double precision)                  AS e_value,
+                    COALESCE(fetch_f_value(sp.ref, sp.task_done_id), 0::double precision) AS f_value,
+                    COALESCE(((SELECT fetch_sprint_plan.is_sprintplan
+                               FROM fetch_sprint_plan(sp.ref) fetch_sprint_plan(project_id, milestone_id, is_sprintplan)))::double precision,
+                             0::double precision)                                         AS plan_value,
+                    COALESCE(((SELECT fetch_sprint_review.is_sprintreview
+                               FROM fetch_sprint_review(sp.ref) fetch_sprint_review(project_id, milestone_id, is_sprintreview)))::double precision,
+                             0::double precision)                                         AS review_value,
+                    COALESCE(((SELECT fetch_sprint_retro.is_sprintretro
+                               FROM fetch_sprint_retro(sp.ref) fetch_sprint_retro(project_id, milestone_id, is_sprintretro)))::double precision,
+                             0::double precision)                                         AS retro_value,
+                    COALESCE(((SELECT fetch_ccs1.coord_counc_s1
+                               FROM fetch_ccs1(sp.ref) fetch_ccs1(project_id, milestone_id, coord_counc_s1)))::double precision,
+                             0::double precision)                                         AS ccs1_value,
+                    COALESCE(((SELECT fetch_ccs2.coord_counc_s2
+                               FROM fetch_ccs2(sp.ref) fetch_ccs2(project_id, milestone_id, coord_counc_s2)))::double precision,
+                             0::double precision)                                         AS ccs2_value
+             FROM scrum_projects sp)
+SELECT cte.project_id,
+       cte.milestone_id,
+       cte.a_value,
+       cte.b_value,
+       cte.c_value,
+       cte.d_value,
+       cte.e_value,
+       cte.f_value,
+       cte.plan_value,
+       cte.review_value,
+       cte.retro_value,
+       cte.ccs1_value,
+       cte.ccs2_value
+FROM cte;
 
-FROM
-    scrum_projects sp)
-
-SELECT project_id,
-       milestone_id,
-       a_value,
-       b_value,
-       c_value,
-       d_value,
-       e_value,
-       r2_value,
-       (a_value*((0.2*c_value)+(0.2*b_value)+(0.6*d_value))*r2_value) AS methode_a1,
-       (a_value*((0.2*c_value)+(0.2*b_value)+(0.6*d_value))*(1+e_value)) AS methode_b1
-
-FROM CTE;
+alter table public.online_scores
+    owner to taiga_u;
 
